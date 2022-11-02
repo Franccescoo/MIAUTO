97bb792c9f8bea57901058596ce3f4d822290416
Franccesco <101402711+Franccescoo@users.noreply.github.com>
2022-11-02 18:49:20 -0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,23 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-//import { Storage } from '@ionic/storage';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
-//import { Camera } from '@awesome-cordova-plugins/camera/ngx';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Camera } from '@awesome-cordova-plugins/camera/ngx';
 //import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-//import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Storage, SQLite, Camera],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
 
-// SQLite, Camera, Geolocation, NativeStorage,Storage
+// SQLite, Camera, Geolocation,Storage
 
 //ESTOS SON IMPORTS DE LO QUE TENIAMOS ANTES, POR SI HAY QUE IMPORTAR ALGO DE NUEVO AQUI ESTA***
